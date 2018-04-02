6c3d34f98e430a689c86afe85bdc717373f7c721
WebsoftProfession <praveen@praveens-mac-mini.local>
2018-04-02 12:40:49 +0530

    Initial Commit

---
+++ WSTipLayerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSTipLayerView
+//
+//  Created by Praveen on 02/04/18.
+//  Copyright © 2018 WebsoftProfession. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSTipLayerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSTipLayerView
+//
+//  Created by Praveen on 02/04/18.
+//  Copyright © 2018 WebsoftProfession. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
